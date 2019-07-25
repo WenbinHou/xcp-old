09bbe49998bc13f4dd8f0d732aa6db6743da7e1a
Wenbin Hou <catchyrime@fastmail.com>
2019-07-25 11:14:37 +0800

    Add thirdparty: cereal
    Commit: https://github.com/USCiLab/cereal/commit/182876d4a34824eb6fbe62502819012d032b67d2

--- thirdparty/NonStandalone.cmake
+++ thirdparty/NonStandalone.cmake
@@ -14,3 +14,12 @@ include_directories("${CMAKE_CURRENT_LIST_DIR}/spdlog/include")
 # We intentionally don't use CMake add_subdirectory,
 # as Visual Studio IDE intellisence doesn't support it well
 include_directories("${CMAKE_CURRENT_LIST_DIR}/CLI11")
+
+
+#===============================================================================
+# cereal
+#===============================================================================
+# We intentionally don't use CMake add_subdirectory,
+# as Visual Studio IDE intellisence doesn't support it well
+add_compile_definitions(CEREAL_THREAD_SAFE=1)
+include_directories("${CMAKE_CURRENT_LIST_DIR}/cereal/include")
--- thirdparty/README.md
+++ thirdparty/README.md
@@ -11,3 +11,9 @@
 - Github: [https://github.com/CLIUtils/CLI11](https://github.com/CLIUtils/CLI11)
 - License: [See here](https://github.com/CLIUtils/CLI11#license)
 - Version: [1.8.0](https://github.com/CLIUtils/CLI11/releases/tag/v1.8.0)
+
+## cereal
+
+- Github: [https://github.com/uSCiLab/cereal](https://github.com/uSCiLab/cereal)
+- License: MIT
+- Version: Commit [182876d4a34824eb6fbe62502819012d032b67d2](https://github.com/uSCiLab/cereal/tree/182876d4a34824eb6fbe62502819012d032b67d2) from branch [develop](https://github.com/uSCiLab/cereal/tree/develop)
