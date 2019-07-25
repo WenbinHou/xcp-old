24660373d3039f81d1e081696710d0272776c6d6
Wenbin Hou <catchyrime@fastmail.com>
2019-07-25 10:45:35 +0800

    Add thirdparty: spdlog
    Commit: https://github.com/gabime/spdlog/commit/01f2438c1f7cfdf8ed2fe9438aa1ad89972f337b

--- thirdparty/NonStandalone.cmake
+++ thirdparty/NonStandalone.cmake
@@ -1,4 +1,14 @@
 #===============================================================================
+# spdlog
+#===============================================================================
+# We use the header-only version, not the compiled version
+#
+# We intentionally don't use CMake add_subdirectory,
+# as Visual Studio IDE intellisence doesn't support it well
+include_directories("${CMAKE_CURRENT_LIST_DIR}/spdlog/include")
+
+
+#===============================================================================
 # CLI11
 #===============================================================================
 # We intentionally don't use CMake add_subdirectory,
--- thirdparty/README.md
+++ thirdparty/README.md
@@ -1,5 +1,11 @@
 # Third-party libraries
 
+## spdlog
+
+- Github: [https://github.com/gabime/spdlog](https://github.com/gabime/spdlog)
+- License: MIT
+- Version: Commit [01f2438c1f7cfdf8ed2fe9438aa1ad89972f337b](https://github.com/gabime/spdlog/tree/01f2438c1f7cfdf8ed2fe9438aa1ad89972f337b) from branch [v1.x](https://github.com/gabime/spdlog/tree/v1.x)
+
 ## CLI11
 
 - Github: [https://github.com/CLIUtils/CLI11](https://github.com/CLIUtils/CLI11)
