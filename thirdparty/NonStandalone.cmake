188571b7282e8d52226d41945511447faaef85eb
Wenbin Hou <catchyrime@fastmail.com>
2019-07-25 10:37:40 +0800

    Add thirdparty: CLI11 1.8.0

---
+++ thirdparty/NonStandalone.cmake
@@ -0,0 +1,6 @@
+#===============================================================================
+# CLI11
+#===============================================================================
+# We intentionally don't use CMake add_subdirectory,
+# as Visual Studio IDE intellisence doesn't support it well
+include_directories("${CMAKE_CURRENT_LIST_DIR}/CLI11")
---
+++ thirdparty/README.md
@@ -0,0 +1,7 @@
+# Third-party libraries
+
+## CLI11
+
+- Github: [https://github.com/CLIUtils/CLI11](https://github.com/CLIUtils/CLI11)
+- License: [See here](https://github.com/CLIUtils/CLI11#license)
+- Version: [1.8.0](https://github.com/CLIUtils/CLI11/releases/tag/v1.8.0)
